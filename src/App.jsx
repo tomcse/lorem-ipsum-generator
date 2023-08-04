fa21359563d284e0ad1f67c6a1944aff84df134d
Tommy <tommychoa@outlook.com>
2023-08-04 17:52:38 +0800

    finished proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,48 @@
+import { useState } from "react";
+import data from "./data";
+import { nanoid } from "nanoid";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, settext] = useState([]);
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tempCount = parseInt(count);
+    const tempText = data.slice(0, tempCount);
+    settext(tempText);
+  };
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="count">Paragraphs:</label>
+        <input
+          type="number"
+          name="count"
+          id="count"
+          value={count}
+          onChange={handleChange}
+          min={1}
+          max={8}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <div className="lorem-text">
+        {text.map((paragraph) => {
+          const id = nanoid();
+          return <p key={id}>{paragraph}</p>;
+        })}
+      </div>
+    </section>
+  );
 };
 export default App;
